b576b4ad1f0bbb9dc7445d7c3c25c04defb9ae25
sefadalklc <sefadalk11@gmail.com>
2026-07-27 22:06:29 +0300

    workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Mevzu-Erol/", // GitHub Pages bu repo alt yolunda yayınlanıyor: eroldalk.github.io/Mevzu-Erol/
+  base: "/Erol-Mevzu/", // GitHub Pages bu repo alt yolunda yayınlanıyor: eroldalk.github.io/Erol-Mevzu/
   server: { port: 5174, strictPort: true },
 });
